4a95548d14bbda0e45efcd487d05b73c39c2efaf
Vladimir Pudovkin <lotreamaun@gmail.com>
2026-01-14 01:01:01 +0300

    docs: Начал писать документацию к файлу работы с БД

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,14 +1,25 @@
 """
-SQLAlchemy sync setup for Movie Tinder Bot
+Настройка SQLAlchemy в синхронном (последовательные операции) режиме
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
 from app.config import settings
 
+"""
+Create_engine(): создает подключение к БД, внутри объекта engine хранятся параметры доступа к БД (URL и тд)
 
-# Engine and Session factory (sync)
+Args:
+    settings.DATABASE_URL — подтягивает URL из файла конфигурации
+    pool_pre_ping=True — проверка соединения перед использованием (важно для прода)
+    future=True - включает интерфейс API SQAlchemy 2.0
+
+Engine создается только один раз — при запуске приложения
+"""
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True, future=True)
+
+"""
+Sessionmaker - фабрика для создания сессий БД
+"""
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
 
 
